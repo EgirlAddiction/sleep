213faa9e298738b35b26c8bcd26830e066b0cf36
jared <87107033+EgirlAddiction@users.noreply.github.com>
2021-09-13 23:46:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const config = require('./config.json');
+// const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('The client is ready!');
 });
 
-client.login(config.token)
+client.login(process.emv.DJS_TOKEN)
