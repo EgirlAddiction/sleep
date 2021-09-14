3b6a543d1ba31b148291746cb62e345ffe87a24a
jared <jj@fbi.ac>
2021-09-14 12:35:59 -0500

    commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 // const config = require('./config.json');
+const command = require('./command')
 
 client.on('ready', () => {
     console.log('The client is ready!');
+
+    command(client, 'ping', message => {
+        message.channel.send('Pong!')
+    })
 });
 
 client.login(process.env.DJS_TOKEN)
